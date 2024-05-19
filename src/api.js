7f8632fedcac9bba135617f4efc932380fff2dcb
achmad fikri <fikriahmat29@gmail.com>
2024-05-19 11:00:04 +0700

    Create a weather application using the openweathermap API

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '686673b3a3msh17e2287103e098cp19c3d8jsn330c4594dd7f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "51ba47457f7876a30b98c95048462823";
